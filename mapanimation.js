8f26644c67a4910574ea23d4e0139ffaea6f793f
JamesLe-MBA <87303337+JamesLe-MBA@users.noreply.github.com>
2021-09-29 23:15:18 -0700

    Update mapanimation.js

--- mapanimation.js
+++ mapanimation.js
@@ -1,53 +1,55 @@
+// This array contains the coordinates for all bus stops between MIT and Harvard
 const busStops = [
-    [-71.093729, 42.359244],
-    [-71.094915, 42.360175],
-    [-71.0958, 42.360698],
-    [-71.099558, 42.362953],
-    [-71.103476, 42.365248],
-    [-71.106067, 42.366806],
-    [-71.108717, 42.368355],
-    [-71.110799, 42.369192],
-    [-71.113095, 42.370218],
-    [-71.115476, 42.372085],
-    [-71.117585, 42.373016],
-    [-71.118625, 42.374863],
+  [-71.093729, 42.359244],
+  [-71.094915, 42.360175],
+  [-71.0958, 42.360698],
+  [-71.099558, 42.362953],
+  [-71.103476, 42.365248],
+  [-71.106067, 42.366806],
+  [-71.108717, 42.368355],
+  [-71.110799, 42.369192],
+  [-71.113095, 42.370218],
+  [-71.115476, 42.372085],
+  [-71.117585, 42.373016],
+  [-71.118625, 42.374863],
 ];
+// const colors = ["RED", "blue", "green", "yellow", "orange", "pink"];
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2ltb24tY2FwcmlsZXMiLCJhIjoiY2tyZmQzZ2hlNXZvcTMxcWh3bTQyNHpsdCJ9.VoAYB8moSu1T9bmTI5pVQg';
+// TODO: add your own access token
+mapboxgl.accessToken = "pk.eyJ1Ijoib3NjYXJ0aWMiLCJhIjoiY2tyMWRpMGt4MjFqcDJ1bW5qbXRiMDAxeCJ9.E-rwj3FOuTi-0IT7AjENnw";
 
+// This is the map instance
 let map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-71.104081, 42.365554],
-    zoom: 14,
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-71.104081, 42.365554],
+  zoom: 14,
 });
 
-var marker = new mapboxgl.Marker({'color': '#red'})
-    .setLngLat([-71.093729, 42.359244])
-    .addTo(map);
+// TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
+var marker = new mapboxgl.Marker()
+  .setLngLat([-71.092761, 42.357575])
+  .addTo(map);
 
+// counter here represents the index of the current bus stop
 let counter = 0;
-
+var colCount = 0;
 function move() {
-    setTimeout(()=>{
-        if (counter >= busStops.length) return;
-        marker.setLngLat(busStops[counter]);
-        counter++;
-        move();
-        update();
-    }, 3000);
+  // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
+  setTimeout(() => {
+    if (counter >= busStops.length) return;
+    marker.setLngLat(busStops[counter]);
+    counter++;
+    move();
+    if (colCount == 5) colcount = 0;
+    marker.style.height = 50;
+    colCount++;
+  }, 1000);
+  // Use counter to access bus stops in the array busStops
+  // Make sure you call move() after you increment the counter.
 }
 
-function update() {
-    setMarkerColor(marker, '#66FF33');
-    setTimeout(()=>{setMarkerColor(marker, '#FFFF00');},1000)
-    setTimeout(()=>{setMarkerColor(marker, '#FF0000');},2000)
-}
-
-function setMarkerColor(marker, color) {
-    let markerElement = marker.getElement();
-    markerElement
-        .querySelectorAll('svg g[fill="' + marker._color + '"]')[0]
-        .setAttribute("fill", color);
-    marker._color = color;
+// Do not edit code past this point
+if (typeof module !== "undefined") {
+  module.exports = { move };
 }
